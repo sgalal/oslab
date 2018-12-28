75bd708b8d77bda4d50c5684df9eabef15b1e3d8
chromezh <chromezh@outlook.com>
2018-12-28 23:25:02 +0800

    Modify style sheet in lab2

--- lab2/README.md
+++ lab2/README.md
@@ -1,4 +1,11 @@
-# ff
+# First Fit Algorithm
+
+## Source Files
+
+* `src/Main.purs`  -- Core library for initializing, allocating and retrieving
+* `ff.html`  -- Web page
+* `ff.css`  -- Style sheet
+* `ff.js`  -- JavaScript code to operate and display
 
 ## Build
 
--- lab2/ff.js
+++ lab2/ff.js
@@ -1,30 +1,32 @@
 var cnt, totalSpace, xs;
 
+window.onload = initialize;
+
 function showRect(leftPercent, thisPercent, isIdle, str) {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
 
     ctx.beginPath();
-    ctx.fillStyle = isIdle ? 'white' : 'aliceblue';
-    ctx.strokeStyle = 'purple';
+    ctx.fillStyle = isIdle ? "white" : "aliceblue";
+    ctx.strokeStyle = "purple";
     ctx.rect(canvas.width * leftPercent, 0, canvas.width * thisPercent, canvas.height);
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
 
     ctx.beginPath();
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = "black";
     ctx.fillText(str, canvas.width * (leftPercent + thisPercent * 0.5), canvas.height * 0.5);
     ctx.closePath();
 }
 
 function initialize() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
     ctx.canvas.width = canvas.width = canvas.offsetWidth;
     ctx.canvas.height = canvas.height = canvas.offsetHeight;
 
-    ctx.font = '120% sans-serif';
+    ctx.font = "110% sans-serif";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
 
@@ -38,33 +40,36 @@ function initialize() {
 function allocate() {
     var ret = PS.Main.allocate(++cnt)(parseInt(document.getElementById("input_allocate").value))(xs);
     if (ret.constructor.name == "Just") {
-        document.getElementById("p_prom").innerText = "";
+        document.getElementById("p_warn").style.display = "none";
         xs = ret.value0;
         resShow(xs);
     } else {
-        document.getElementById("p_prom").innerText = "You cannot do that!";
+        document.getElementById("p_warn").style.display = "inherit";
     }
+    document.getElementById("input_allocate").value = "";
 }
 
 function retrieve() {
     var ret = PS.Main.retrieve(parseInt(document.getElementById("input_pid").value))(xs);
     if (ret.constructor.name == "Just") {
-        document.getElementById("p_prom").innerText = "";
+        document.getElementById("p_warn").style.display = "none";
         xs = ret.value0;
         resShow(xs);
     } else {
-        document.getElementById("p_prom").innerText = "You cannot do that!";
+        document.getElementById("p_warn").style.display = "inherit";
     }
+    document.getElementById("input_pid").value = "";
 }
 
 function resShow(arr) {
     var acc = 0;
     for (var i = 0; i < arr.length; i++) {
         var pid = arr[i].value0.pid;
+        var len = arr[i].value0.len;
         if (pid) {
-            showRect(acc, arr[i].value0.len / totalSpace, true, arr[i].value0.len + "[" + pid + "]");
+            showRect(acc, len / totalSpace, true, len + "(" + pid + ")");
         } else {
-            showRect(acc, arr[i].value0.len / totalSpace, false, arr[i].value0.len);
+            showRect(acc, len / totalSpace, false, len);
         }
         acc += arr[i].value0.len / totalSpace;
     }
